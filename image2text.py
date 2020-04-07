233597317535d9c077888320d3b09938ed10737e
pprashanti <p.prashanti1018@gmail.com>
2020-04-08 02:10:12 +0530

    Commited OCR_Image_to_Text

---
+++ image2text.py
@@ -0,0 +1,9 @@
+import pytesseract
+from PIL import Image
+
+pytesseract.pytesseract.tesseract_cmd= r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+
+img=Image.open('pic.png')
+#img=Image.open('images.png')
+text=pytesseract.image_to_string(img)
+print(text)
